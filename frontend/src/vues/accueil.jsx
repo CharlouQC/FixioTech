c69bad6064ddc6eb34e0664d4b7a31c14bc2e64e
CHARLES TREPANIER <2233341@SERVEUR.CA>
2025-09-19 15:30:48 -0400

    Initialisation du projet + création fichiers de base
    Co-authored-by: bastiendeveault@gmail.com <bastiendeveault@gmail.com>

---
+++ frontend/src/vues/accueil.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./accueil.css";
+
+const Accueil = () => {
+  return (
+    <div className="accueil-container">
+      <p>
+        
+      </p>
+      <h1>Bienvenue sur FixioTech !</h1>  
+    
+    </div>
+  );
+};
+
+export default Accueil;
