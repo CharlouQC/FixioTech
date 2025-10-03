7f449429acaaaad5c7e7d769b2b8a1c518a44008
CHARLES TREPANIER <2233341@SERVEUR.CA>
2025-10-03 15:31:49 -0400

    [Upd] Update - Modification du fichier App.jsx pour l'ajout de la page Services (Route)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import MainNavigation from './vues/Navigation/MainNavigation';
-import Accueil from './vues/accueil';
-import Login from './vues/login';
-import Inscription from './vues/inscription';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import MainNavigation from "./vues/Navigation/MainNavigation";
+import Accueil from "./vues/accueil";
+import Login from "./vues/login";
+import Inscription from "./vues/inscription";
+import Services from "./vues/services_aides";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Accueil />} />
             <Route path="/login" element={<Login />} />
             <Route path="/inscription" element={<Inscription />} />
+            <Route path="/services_aides" element={<Services />} />
           </Routes>
         </main>
       </div>
